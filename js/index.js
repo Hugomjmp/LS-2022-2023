2ba7abd651916f35593946a49b2ca290080830d8
Hugo Pereira <80553701+Hugomjmp@users.noreply.github.com>
2023-04-26 17:51:53 +0100

    modified:   css/component.css 	modified:   index.html 	modified:   js/index.js

--- js/index.js
+++ js/index.js
@@ -1,8 +1,11 @@
 function LinguagensScript() {
-  return <h1>Ultimate Tic-Tac-Toe</h1>;
+  return <h1 class="titulo">Ultimate Tic-Tac-Toe</h1>;
 }
 function Welcome(){
-
+    return <p class="Menu">WELCOME AQUI!!</p>
+}
+function Jogo(){
+  return <p>JOGO AQUI!!</p>
 }
 function Footer(){
     return <footer>LinguagensScript 2022/2023 @ Hugomdjmp & PedroAlves</footer>;
@@ -10,9 +13,9 @@ function Footer(){
 const containerRoot = document.getElementById("root");
 ReactDOM.render(
   <React.StrictMode>
-    <LinguagensScript/>
-
-    <Footer/>
+    <LinguagensScript/>{/*Para aparecer o título*/}
+    <Welcome/>
+    <Footer/> {/*Para aparecer o Footer*/}
   </React.StrictMode>,
   containerRoot
 );

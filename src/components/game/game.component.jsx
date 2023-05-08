39e9872ce194292457bb00b444ce876ef83cf220
Hugo Pereira <80553701+Hugomjmp@users.noreply.github.com>
2023-05-08 18:45:10 +0100

    tabuleiro

--- src/components/game/game.component.jsx
+++ src/components/game/game.component.jsx
@@ -4,21 +4,345 @@ function Game() {
   return (
     <div>
       <div className="linha_1_a_3 row border-bottom border-white border-5">
-        <div className="Tabuleiro_1 col border-end border-white border-5"><p className="test">X</p></div>
-        <div className="Tabuleiro_2 col border-end border-white border-5"><p className="test">O</p></div>
-        <div className="Tabuleiro_3 col"><p className="test">X</p></div>
+        <div className="Tabuleiro_1 col border-end border-white border-5">
+          <div className="linha_1_a_3 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_4_a_6 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_7_a_9 row">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">O</p>
+            </div>
+          </div>
+        </div>
+        <div className="Tabuleiro_2 col border-end border-white border-5">
+          <div className="linha_1_a_3 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_4_a_6 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_7_a_9 row">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">O</p>
+            </div>
+          </div>
+        </div>
+        <div className="Tabuleiro_3 col">
+          <div className="linha_1_a_3 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_4_a_6 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_7_a_9 row">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">O</p>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="linha_4_a_6 row border-bottom border-white border-5">
-        <div className="Tabuleiro_1 col border-end border-white border-5"><p className="test">O</p></div>
-        <div className="Tabuleiro_2 col border-end border-white border-5"><p className="test">O</p></div>
-        <div className="Tabuleiro_3 col"><p className="test">X</p></div>
+        <div className="Tabuleiro_1 col border-end border-white border-5">
+          <div className="linha_1_a_3 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_4_a_6 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_7_a_9 row">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">O</p>
+            </div>
+          </div>
+        </div>
+        <div className="Tabuleiro_2 col border-end border-white border-5">
+          <div className="linha_1_a_3 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_4_a_6 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_7_a_9 row">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">O</p>
+            </div>
+          </div>
+        </div>
+        <div className="Tabuleiro_3 col">
+          <div className="linha_1_a_3 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_4_a_6 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_7_a_9 row">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">O</p>
+            </div>
+          </div>
+        </div>
       </div>
 
       <div className="linha_7_a_9 row">
-        <div className="Tabuleiro_1 col border-end border-white border-5"><p className="test">X</p></div>
-        <div className="Tabuleiro_2 col border-end border-white border-5"><p className="test">X</p></div>
-        <div className="Tabuleiro_3 col"><p className="test">O</p></div>
+        <div className="Tabuleiro_1 col border-end border-white border-5">
+          <div className="linha_1_a_3 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_4_a_6 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_7_a_9 row">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">O</p>
+            </div>
+          </div>
+        </div>
+        <div className="Tabuleiro_2 col border-end border-white border-5">
+          <div className="linha_1_a_3 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_4_a_6 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_7_a_9 row">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">O</p>
+            </div>
+          </div>
+        </div>
+        <div className="Tabuleiro_3 col">
+          <div className="linha_1_a_3 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_4_a_6 row border-bottom border-white border-5">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">O</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">X</p>
+            </div>
+          </div>
+
+          <div className="linha_7_a_9 row">
+            <div className="Tabuleiro_1 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_2 col border-end border-white border-5">
+              <p className="test">X</p>
+            </div>
+            <div className="Tabuleiro_3 col">
+              <p className="test">O</p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
